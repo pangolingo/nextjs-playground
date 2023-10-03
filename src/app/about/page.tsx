adbe7891ba3aaf2d6557a557513f3bf06acd0d96
Dave Iverson <dave@ivrsn.com>
2023-10-02 21:18:43 -0500

    Set up the Home and About pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import PikaDialog from "@/components/PikaDialog";
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <>
+      <h2 className="h2">About Pokeland</h2>
+
+      <h3 className="h3 my-4">I love pokemons!</h3>
+      <div className="space-y-4">
+        <p>
+          This website was made to share my love of pokemons with the world.
+          Take a look at ALL the pokemon, and save your favorites.
+        </p>
+
+        <p>If you like this site, let me know.</p>
+
+        <p>
+          <Link className="btn inline-block" href="/contact">
+            Tell me you like this site
+          </Link>
+        </p>
+
+        <p>
+          <PikaDialog />
+        </p>
+      </div>
+    </>
+  );
+}
---
+++ src/components/PikaDialog.tsx
@@ -0,0 +1,10 @@
+const pika = () => {
+  alert("PIKA PIKA!");
+};
+export default function PikaDialog() {
+  return (
+    <button className="btn" onClick={pika}>
+      PIKA!
+    </button>
+  );
+}
